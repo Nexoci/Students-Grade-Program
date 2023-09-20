3964726e5496519f054bbcfee53d9ebe8c822be5
Nexoci <konnork84@kprschools.ca>
2023-09-20 13:24:14 -0400

    The Beggining Of The File

---
+++ Grades.py
@@ -0,0 +1,7 @@
+#Made By: Konnor Kobelka
+#Created: Sep 19th
+#Purpose: A Mini Student Grade Book
+Grades= [['Marquise','34','77','63','89'],['Johnathan','89','93','97','88'],['Mary Von Bock','9','33','37','12'],['Gary','77','60','83','59']]
+print("Welcome To The Class Report Card")
+for row in Grades:
+    print(f"Name:{row[0]} English:{row[1]}")
